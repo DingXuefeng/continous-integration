5c43c5bedc8f202d53f3b846fd43b30e613e21be
Xuefeng, Ding <xuefeng.ding.physics@gmail.com>
2022-03-05 12:37:47 -0500

    Add init main.
    Signed-off-by: Xuefeng, Ding <xuefeng.ding.physics@gmail.com>

--- main.cc
+++ main.cc
@@ -8,3 +8,9 @@
 // All rights reserved. 2022 copyrighted.
 /*****************************************************************************/
 
+#include <iostream>
+
+int main(void) {
+    std::cout<<"Hello world"<<std::endl;
+    return 0;
+}
