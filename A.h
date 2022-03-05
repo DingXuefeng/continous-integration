04339ed7fc224798bc2fff8e804662c23912aa24
Xuefeng, Ding <xuefeng.ding.physics@gmail.com>
2022-03-05 12:21:10 -0500

    test pre-commit, init commit

---
+++ A.h
@@ -0,0 +1,20 @@
+/*****************************************************************************/
+// Author: Xuefeng Ding <xuefengd@princeton.edu>
+// Institute: Department of Physics, Princeton University
+// Date: 3/5/22
+// Version: v1.0
+// Description: GooStats, a statistical analysis toolkit that runs on GPU.
+//
+// All rights reserved. 2022 copyrighted.
+/*****************************************************************************/
+
+#ifndef CONTINOUS_INTEGRATION_A_H
+#define CONTINOUS_INTEGRATION_A_H
+
+
+class A {
+
+};
+
+
+#endif //CONTINOUS_INTEGRATION_A_H
